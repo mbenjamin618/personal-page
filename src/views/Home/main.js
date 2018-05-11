0650540db8007479cda5f62a8c10755505a187e8
mbenjamin618 <matthew@saywerk.com>
2018-05-11 09:09:44 -0400

    home page styling

--- src/views/Home/main.js
+++ src/views/Home/main.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import '../../scss/views/home.css'
 import MattAvatar from '../../assets/matthew-avatar.jpg'
-console.log(MattAvatar)
+
 const Main = (props) => (
   <div className="home container">
     <div className="row h-100">
-      <div className="col-4 offset-4">
-        <div className="d-flex h-100 justify-content-center align-items-center">
-          <img src={MattAvatar} className="img-fluid" alt="Matt Avatar" />
+      <div className="col-12">
+        <div className="d-flex h-100 justify-content-between align-items-center">
+          <div className="menu-item menu-item--left order-1">
+            <i className="material-icons">keyboard_arrow_left</i>
+            <span>About Me</span>
+          </div>
+          <div className="menu-item menu-item--right order-3">
+            <span>My Work</span>
+            <i className="material-icons">keyboard_arrow_right</i>
+          </div>
+          <div className="matt-image-wrapper order-2">
+            <img src={MattAvatar} className="matt-image" alt="Matt Avatar" />
+          </div>
         </div>
       </div>
     </div>
